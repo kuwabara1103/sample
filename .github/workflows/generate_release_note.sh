127d77fac28de2c7d3eb5d922468e849adcadf96
kuwabara <kuwabara@sansan.com>
2023-04-11 17:03:55 +0900

    余分なファイル削除

--- .github/workflows/generate_release_note.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# get latest release tag
-TAG=$(git describe --tags --abbrev=0)
-
-# generate release note
-git log --pretty=format:'- %s [View on GitHub](https://github.com/your-username/your-repo/commit/%H)' ${TAG}..HEAD > release_note.txt
